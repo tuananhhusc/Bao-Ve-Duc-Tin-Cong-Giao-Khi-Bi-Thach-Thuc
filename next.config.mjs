a4f156cfeda4ba0e1e7702d03be5b385fab9e1ca
Your Name <you@example.com>
2026-03-25 12:49:54 +0700

    Rename next.config.ts to next.config.mjs to avoid Github Actions configure-pages overwrite overriding MDX config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 import createMDX from "@next/mdx";
-import type { NextConfig } from "next";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   pageExtensions: ["ts", "tsx", "mdx"],
   output: "export",
   images: {
